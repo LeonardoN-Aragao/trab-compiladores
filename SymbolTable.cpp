2f007c17275204c2e6761c101d053506a53d7d9f
Juliana <julianadepaula001@gmail.com>
2022-10-12 20:54:06 -0300

    add initial symbol table

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -6,29 +6,68 @@
 #include <fstream>
 #include <vector>
 #include "ListaEncad.h"
+#include <math.h>
+#include <string>
+#include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-TabelaHash::TabelaHash(int tam){
+TabelaHash::TabelaHash(int tam)
+{
     this->tam = tam;
-    tabela = (ListaEncad**) malloc(sizeof(ListaEncad*)*tam);
-    for(int i=0;i<tam;i++)
+    tabela = (ListaEncad **)malloc(sizeof(ListaEncad *) * tam);
+    for (int i = 0; i < tam; i++)
     {
         tabela[i] = new ListaEncad();
     }
 }
 
-int TabelaHash::hashFunction(string id,int j){
-    int intId = converteId(id);
-    long long k = (intId + j*j);
-    int key = k % tam;
+// int TabelaHash::hashFunction(string key)
+// {
+//     int result = 0;
+//     for (size_t i = 0; i < key.length(); ++i)
+//     {
+//         result += key[i] * pow(13, i);
+//     }
+//     cout << "o result: " << fabs(result) << endl;
+//     return 30;
+//     return 3;
+// }
 
-        return key;
+int converteId(string id)
+{
+    int soma = 0;
+    for (int i = 0; i < id.length(); i++)
+    {
+        soma += id[i];
+    }
+    return soma;
 }
 
-void TabelaHash::insere(string key){
-    int i = hashFunction(key);
-    No *novoArtista = new No(key, i, NULL);
-    tabela[i]->insereFinal(novoArtista)
+int TabelaHash::hashFunction(string key)
+{
+    int intId = converteId(key);
+    int k = intId % tam;
+    return k;
 }
 
+void TabelaHash::insere(string key)
+{
+    int index;
+    index = hashFunction(key);
+    cout << "indice: " << index << endl;
+    No *p = new No(index, key, NULL);
+    tabela[index]->insereFinal(p);
+    cout << "inseriu" << endl;
+}
+
+void TabelaHash::print()
+{
+    for (int i = 0; i < tam; i++)
+    {
+        cout << i << " ";
+        tabela[i]->print();
+        cout << endl;
+    }
+}
