79ca2ad7e074ecf7301ac6e3218fba7ad3d5a4d0
Juliana <julianadepaula001@gmail.com>
2022-10-15 17:26:30 -0300

    merge the files into SymbolTable

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -5,25 +5,122 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
-#include "ListaEncad.h"
 #include <math.h>
-#include <string>
-#include <iostream>
 #include <cstdlib>
-
 using namespace std;
 
-TabelaHash::TabelaHash(int tam)
+LinkedList::LinkedList()
 {
-    this->tam = tam;
-    tabela = (ListaEncad **)malloc(sizeof(ListaEncad *) * tam);
-    for (int i = 0; i < tam; i++)
+    cout << "Criando objeto LinkedList" << endl;
+    first = NULL;
+    last = NULL;
+    n = 0;
+}
+
+LinkedList::~LinkedList()
+{
+    cout << "Destruindo objeto LinkedList" << endl;
+    No *p = first;
+    while (p != NULL)
     {
-        tabela[i] = new ListaEncad();
+        No *t = p->getProx();
+        delete p;
+        p = t;
     }
 }
 
-// int TabelaHash::hashFunction(string key)
+void LinkedList::print()
+{
+    No *p;
+    for (p = first; p != NULL; p = p->getProx())
+        cout << "-> "
+             << "(" << (p->getToken()) << ", " << p->getKey() << ")";
+}
+
+int LinkedList::get(int k)
+{
+    No *p = first;
+    int i = 0;
+    while (i < k && p != NULL)
+    {
+        i++;
+        p = p->getProx();
+    }
+    if (p == NULL)
+    {
+        cout << "ERRO: Indice invalido!" << endl;
+        exit(1);
+    }
+    else
+        return p->getToken();
+}
+
+
+void LinkedList::insereFinal(No *p)
+{
+    if (last != NULL)
+        last->setProx(p);
+
+    last = p;
+
+    n++;
+    if (n == 1)
+        first = p;
+}
+
+void LinkedList::removeInicio()
+{
+    No *p;
+    if (first != NULL)
+    {
+        p = first;
+        first = p->getProx();
+        delete p;
+
+        n--;
+        if (n == 0)
+            last = NULL;
+    }
+    else
+        cout << "ERRO: lista vazia!" << endl;
+}
+
+void LinkedList::removeFinal()
+{
+    No *p;
+    if (last != NULL)
+    {
+        if (first == last)
+        {
+            first = NULL;
+            p = NULL;
+        }
+        else
+        {
+            p = first;
+            while (p->getProx() != last)
+                p = p->getProx();
+            p->setProx(NULL);
+        }
+        delete last;
+        last = p;
+        n--;
+    }
+    else
+        cout << "ERRO: lista vazia!" << endl;
+}
+
+HashTable::HashTable(int tam)
+{
+    this->tam = tam;
+    table = (LinkedList **)malloc(sizeof(LinkedList *) * tam);
+    for (int i = 0; i < tam; i++)
+    {
+        table[i] = new LinkedList();
+    }
+}
+
+// int HashTable::hashFunction(string key)
 // {
 //     int result = 0;
 //     for (size_t i = 0; i < key.length(); ++i)
@@ -45,29 +142,29 @@ int converteId(string id)
     return soma;
 }
 
-int TabelaHash::hashFunction(string key)
+int HashTable::hashFunction(string key)
 {
     int intId = converteId(key);
     int k = intId % tam;
     return k;
 }
 
-void TabelaHash::insere(string key)
+void HashTable::insert(string key)
 {
     int index;
     index = hashFunction(key);
     cout << "indice: " << index << endl;
     No *p = new No(index, key, NULL);
-    tabela[index]->insereFinal(p);
+    table[index]->insereFinal(p);
     cout << "inseriu" << endl;
 }
 
-void TabelaHash::print()
+void HashTable::print()
 {
     for (int i = 0; i < tam; i++)
     {
         cout << i << " ";
-        tabela[i]->print();
+        table[i]->print();
         cout << endl;
     }
 }
--- SymbolTable.h
+++ SymbolTable.h
@@ -3,25 +3,62 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include "No.h"
-#include "ListaEncad.h"
+#include <cstdlib>
 
 typedef std::string string;
-class TabelaHash
+using namespace std;
+
+class No
+{
+private:
+    int token; /// valor armazenado no No
+    string key;
+    No *prox; /// ponteiro para o proximo No
+
+public:
+    No(int token, string key, No *prox)
+    {
+        this->token = token;
+        this->key = key;
+        this->prox = prox;
+    };
+    ~No(){};
+    string getKey() { return key; };
+    int getToken() { return token; };
+    No *getProx() { return prox; };
+    void setKey(string val) { key = val; };
+    void setToken(int val) { token = val; };
+    void setProx(No *p) { prox = p; };
+};
+
+class LinkedList
 {
 public:
-    TabelaHash(int tam);
-    void insere(string key);
+    LinkedList();
+    ~LinkedList();
+    int get(int k);           /// retorna o valor do k-�simo elemento
+    void insereFinal(No *p);  /// insere um novo No contendo val no final da lista
+    void removeInicio();      /// remove o first No da lista
+    void removeFinal();       /// remove o �ltimo No da lista
+    void print();
+
+private:
+    No *first; /// ponteiro para o first No da lista
+    No *last;   /// ponteiro para o last No da lista
+    int n;        /// numero de nos na lista
+};
+
+class HashTable
+{
+public:
+    HashTable(int tam);
+    void insert(string key);
     void print();
 
 private:
     int tam;
-    int ocupacao;
     int hashFunction(string);
-    ListaEncad **tabela;
+    LinkedList **table;
 };
-
-
 
 #endif // HASH_H_INCLUDED
